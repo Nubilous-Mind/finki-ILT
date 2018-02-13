b8316d26aa495c2390740a639cc9b684343039d7
Marjan <marjan.ristov@hotmail.com>
2018-02-13 01:50:14 +0100

    Changing routes dir

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mysql = require('mysql');
 var app = express();
 var path = require('path');
-var hike = require('./routes/hike');
+var hike = require('routes/hike');
 
 app.get('/hikes', hike.index);
 app.post('/add_hike', hike.add_hike);
