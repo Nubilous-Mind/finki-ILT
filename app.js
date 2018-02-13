cd4e56a6c7228b4f66409f868a28d47eca384c15
Marjan <marjan.ristov@hotmail.com>
2018-02-13 01:31:40 +0100

    Testing static files.

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var mysql = require('mysql');
 var app = express();
 var path = require('path');
+var hike = require('./routes/hike');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/hikes', hike.index);
+app.post('/add_hike', hike.add_hike);
 
 app.listen(8081, function(){
   console.log("Listening..");
