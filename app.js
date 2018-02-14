93b7584fc9b291591f0a9ebec1c7a741a60230cc
Marjan <marjan.ristov@hotmail.com>
2018-02-14 23:13:51 +0100

    Fixed query

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ connection.connect(function(err) {
     console.error('Database connection failed: ' + err.stack);
     return;
   }
-  connection.query("select * from worker", function (err, result, fields) {
+  connection.query("select * from worker", function (errorQuery, result, fields) {
     // if any error while executing above query, throw error
-      if (err) throw err;
+      if (errorQuery) throw errorQuery;
     // if there is no error, you have the result
       console.log(result);
   });                
